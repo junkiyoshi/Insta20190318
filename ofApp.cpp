016d156d27ef3712c4f79e783f1d61d00654de5a
中内 純 <nakauchi.k@g-hits.co.jp>
2019-03-18 18:55:49 +0900

    First

---
+++ ofApp.cpp
@@ -0,0 +1,41 @@
+﻿#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openframeworks");
+
+	ofBackground(239);
+	ofSetColor(39);
+	ofSetLineWidth(3);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	int span = 60;
+	for (int x = span * 0.5; x < ofGetWidth(); x += span) {
+
+		for (int y = span * 0.5; y < ofGetHeight(); y += span) {
+
+			int len = ofMap(ofNoise(x * 0.005, y * 0.005, ofGetFrameNum() * 0.01), 0, 1, 0, span * 0.75);
+
+			ofDrawLine(x - len, y, x + len, y);
+			ofDrawLine(x, y - len, x, y + len);
+		}
+	}
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
